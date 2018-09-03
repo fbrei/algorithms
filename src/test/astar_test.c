4820da6b3c14c39214b3515a1426182c9f0df687
Felix Brei <felix.brei@posteo.de>
2018-09-03 22:40:58 +0200

    Some changes to accomodate new methods in astar.h

--- src/test/astar_test.c
+++ src/test/astar_test.c
@@ -21,40 +21,38 @@ double heuristic(void *n1, void *n2)  {
 
 }
 
-int equals(void *first, void *second) {
-
-  MapNode *m1 = (MapNode*) first;
-  MapNode *m2 = (MapNode*) second;
-
-  return (m1->x == m2->x && m1->y == m2->y) ? 1 : 0;
-
-}
-
-
 void print_node(AStarPathNode *a) {
 
   MapNode *m = (MapNode*) a->data;
 
-  printf("%5.2lf %5.2lf ", m->x, m->y);
+  printf("%5.2lf %5.2lf [%5.2lf]", m->x, m->y, a->estimate + a->total_dist);
 
 }
 
 int main(int argc, const char** argv) {
 
-  MapNode start = { .x = 1, .y = 2 };
-  MapNode m1 = { .x = 3, .y = 3 };
-  MapNode goal = { .x = 10, .y = 4 };
+  MapNode start = { .x = 3, .y = 5 };
+  MapNode m1 = { .x = 5, .y = 4 };
+  MapNode m2 = { .x = 5, .y = 6 };
+  MapNode goal = { .x = 10, .y = 5 };
 
   Graph *g = graph_init(GRAPH_DIRECTED);
 
   graph_add(g, &start);
   graph_add(g, &m1);
+  graph_add(g, &m2);
   graph_add(g, &goal);
 
   graph_connect(g, &start, &m1, heuristic(&start,&m1));
-  graph_connect(g, &m1, &goal, heuristic(&m1,&goal));
+  graph_connect(g, &start, &m2, heuristic(&start,&m2));
 
-  AStarPathNode *a = astar(g,&start,&goal,heuristic,equals);
+  graph_connect(g, &m1, &m2, heuristic(&m1,&m2));
+
+  graph_connect(g, &m1, &goal, heuristic(&m1,&goal));
+  graph_connect(g, &m2, &goal, heuristic(&m2,&goal));
+
+  AStarPathNode *a = astar(g,&start,&goal,heuristic,NULL);
+  AStarPathNode *path = a;
   
   do {
     print_node(a);
@@ -62,6 +60,7 @@ int main(int argc, const char** argv) {
     a = a->parent;
   } while (a != NULL);
 
+  astar_free_path(path);
   graph_destroy(g,NULL);
 
   return 0;
