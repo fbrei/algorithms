a4002aea215c787e7fe6492ab4b0cf3605bbb558
Felix Brei <felix.brei@posteo.de>
2018-09-08 17:39:02 +0200

    Removed linear functions since it is not needed

--- include/tangents.h
+++ include/tangents.h
@@ -16,19 +16,6 @@
 
 #include "dtype.h"
 
-/**
- * \brief Describes a linear function by its slope and y-intercept
- *
- * Since all tangents are linear functions it makes sense to create this
- * struct to group the results together. All functions in this module
- * will return a DArray containing exactly this datatype.
- *
- */
-typedef struct _LinearFunction {
-  double m; 		/**< Slope */
-  double n; 		/**< y-intercept */
-} LinearFunction;
-
 
 /** \brief Describes a point in a 2D-plant
  *
@@ -49,27 +36,6 @@ typedef struct _CircularObstacle {
   MapPoint position;
   double radius;
 } CircularObstacle;
-
-
-
-/**
- * \brief Calculates the tangents on a circle given an external point
- *
- * Please note that this method will fail if the y-coordinate of the center
- * of the circle and the external point are the same, due to internal
- * calculations. This will be fixed in a later version and is currently
- * one of the top priorities.
- * 
- * \param point_x x-coordinate of the point
- * \param point_y y-coordinate of the point
- * \param circ_x x-coordinate of the circle
- * \param circ_y y-coordinate of the circle
- * \param circ_r Radius of the circle
- * \return A DArray containing the results
- */
-DArray* tangent_circle_point(double point_x, double point_y,
-    double circ_x, double circ_y, double circ_r);
-
 
 
 /**
--- src/tangents.c
+++ src/tangents.c
@@ -1,70 +1,8 @@
 #include "tangents.h"
 
 #include <stdio.h>
-
 #include <math.h>
 
-DArray* tangent_circle_point(double point_x, double point_y, double circ_x, double circ_y, double circ_r) {
-
-
-  // Point in the middle between P and center of circle
-  double center_x = (point_x + circ_x) / 2;
-  double center_y = (point_y + circ_y) / 2;
-
-
-  // Radius of Thales circle
-  double center_r = sqrt( (center_x - circ_x) * (center_x - circ_x) + (center_y - circ_y) * (center_y - circ_y) );
-
-
-  // Intermediate value for calculating the polar for P
-  //
-  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  // This is the area where a fix needs to be applied
-  double a = (circ_x * circ_x) + (circ_y * circ_y) - (circ_r * circ_r);
-  a -= ( (center_x * center_x) + (center_y * center_y) - (center_r * center_r) );
-
-  double div = circ_y - center_y;
-
-  double m = (center_x - circ_x) / div;
-  double n = a / (2.0 * div);
-
-  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  // x^2 + px + q = 0:
-  
-  a = (m*m + 1);
-
-  double p = -2.0 * (circ_x - m * n + m * circ_y) / a;
-  double q = (n * n + circ_x * circ_x - 2.0 * n * circ_y + circ_y * circ_y - circ_r * circ_r) / a;
-
-  // Now solve the equation
-  double sqrt_q = sqrt((p*p) / 4 - q);
-
-  double x1 = -p/2 + sqrt_q, x2 = -p/2 - sqrt_q;
-  double y1 = m * x1 + n, y2 = m * x2 + n;
-
-  double t1_m = (point_y - y1) / (point_x - x1);
-  double t1_n = y1 - t1_m * x1;
-
-  double t2_m = (point_y - y2) / (point_x - x2);
-  double t2_n = y2 - t2_m * x2;
-
-  DArray *result = darray_init();
-
-  LinearFunction *t1 = malloc(sizeof(LinearFunction));
-  t1->m = t1_m;
-  t1->n = t1_n;
-
-  LinearFunction *t2 = malloc(sizeof(LinearFunction));
-  t2->m = t2_m;
-  t2->n = t2_n;
-
-  darray_set(result, t1, 0);
-  darray_set(result, t2, 1);
-
-  return result;
-
-}
 
 DArray* tangent_circle_point_intersects(double point_x, double point_y, double circ_x, double circ_y, double circ_r) {
 
